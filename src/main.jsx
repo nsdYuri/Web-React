8e57803f4c204d0e82840a28ec8bcf7a7fdd4213
Yuri <yurichichedom808@gmail.com>
2023-09-05 20:37:28 -0300

    Importando paginas no arquivo main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Aparelhos from './routes/Aparelhos.jsx'
+import Error404 from './router/Error404.jsx'
+import VisualizarAparelho from './router/VisualizarAparelho.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
