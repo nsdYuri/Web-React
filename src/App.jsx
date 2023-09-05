d2b361083610d8e85ed95a15ed95905fbda8f161
Yuri <yurichichedom808@gmail.com>
2023-09-05 14:27:19 -0300

    testando

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Coded By Yuri | Rm552262</h1>
+      <h1>Vite + React Coded By Yuri | Rm 552262</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
