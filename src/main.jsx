051eed33fdff7fadeb81f665c9874d975d87124f
Yuri <yurichichedom808@gmail.com>
2023-09-05 22:26:50 -0300

    Configurando rota para as paginas

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './routes/Home.jsx'
-import Aparelhos from './routes/Aparelhos.jsx'
-import Error404 from './router/Error404.jsx'
-import VisualizarAparelho from './router/VisualizarAparelho.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './router/Home.jsx';
+import Aparelhos from './router/Aparelhos.jsx';
+import VisualizarAparelho from './router/VisualizarAparelho.jsx';
+import Error404 from './router/Error404.jsx';
+
+const routes = createBrowserRouter([
+  { path: '/', element: <App />, errorElemnt:<Error404/>, children: [
+      {path: '/', element: <Home/>},
+      {path: '/aparelhos', elemet: <Aparelhos/>},
+      {path: '/aprelho/:id', element: <VisualizarAparelho/>}
+    ]  
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </React.StrictMode>,
 )
