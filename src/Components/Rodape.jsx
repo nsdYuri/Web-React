86b5a740168554331cb0be6dd377522b2fc55a3b
Yuri <yurichichedom808@gmail.com>
2023-09-06 05:26:28 -0300

    rodapé criado

---
+++ src/Components/Rodape.jsx
@@ -0,0 +1,9 @@
+export default function Rodape(){
+    return(
+        <>
+            <footer>
+                <p>&copy; 2023 Motorola Mobility LLC. Todos os direitos reservados.</p>
+            </footer>
+        </>
+    )
+}
